f3f05fe4e26fe769f5534dec8ff2356104f5fd4a
alok-0204 <25110cn393@glbitm.ac.in>
2026-02-23 15:05:33 +0530

    Add files via upload

---
+++ sum_of_no_upto_n_terms.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+    int n,sum=0;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    for( int i=0;i<=n;i++) {
+        sum+=i;
+    }
+    printf("The Sum upto given no. is : %d",sum);
+    return 0;
+}
